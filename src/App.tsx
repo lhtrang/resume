decd528c807eb7f6ca0d6d45caab12a26bcff2b2
Trang Le <trangle@b13technology.com>
2021-03-27 22:36:46 +0700

    auto calculate deploy time.

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,19 @@ import React from 'react';
 import { Redirect, Route } from 'react-router-dom';
 import 'primereact/resources/primereact.min.css';
 import 'primeicons/primeicons.css';
-import './App.css';
+import './App.scss';
 import { AboutMe } from './pages/about-me/AboutMe';
 
 export class App extends React.Component {
 
 	render() {
 		return (
-			<main className="App">
-				<Route exact path="/" component={AboutMe}/>
-				<Redirect to="/"/>
-			</main>
+			<div className="app">
+				<main>
+					<Route exact path="/" component={AboutMe}/>
+					<Redirect to="/"/>
+				</main>
+			</div>
 		);
 	}
 }
